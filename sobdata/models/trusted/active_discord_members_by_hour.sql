d6d3b91f44cc21c5c176683f52d709fa8b9a3b9d
Andre Anastácio <andreluisanastacio@gmail.com>
2022-10-16 13:29:51 -0300

    Fix active discord members by hour metric

--- sobdata/models/trusted/active_discord_members_by_hour.sql
+++ sobdata/models/trusted/active_discord_members_by_hour.sql
@@ -1,22 +1,15 @@
 {{ config(materialized='table') }}
 
-WITH members_by_hour AS (
-  SELECT date_trunc('hour', extracted_at) AS hour
-        ,AVG(approximate_presence_count) AS avg_presence
-   FROM {{ ref('discord_members') }}
-   GROUP BY 1
-),
-
-members_by_hour_and_weekday AS (
-  SELECT EXTRACT('ISODOW' FROM hour)::int AS isodow
-        ,hour
-        ,AVG(avg_presence)::int  AS avg_members
-  FROM members_by_hour
+WITH members_by_hour_and_weekday AS (
+  SELECT isodow
+        ,hour_interval
+        ,AVG(approximate_presence_count)::int  AS avg_members
+  FROM {{ ref('discord_members') }}
   GROUP BY 1, 2
 )
 
 SELECT dw.name as week_name
-      ,hour
+      ,hour_interval
       ,mw.avg_members
 FROM members_by_hour_and_weekday mw
 JOIN {{ ref('day_of_the_week') }} dw ON mw.isodow = dw.isodow
--- sobdata/models/trusted/discord_members.sql
+++ sobdata/models/trusted/discord_members.sql
@@ -1,6 +1,18 @@
 {{ config(materialized='table') }}
 
-SELECT _started_at::timestamptz AT TIME ZONE 'America/Sao_Paulo' as extracted_at
-      ,CAST(approximate_member_count AS INTEGER)
-      ,CAST(approximate_presence_count AS INTEGER)
-FROM {{ source('public', 'discord_data') }}
+WITH coerced_types AS (
+  SELECT _started_at::timestamptz AT TIME ZONE 'America/Sao_Paulo' as extracted_at
+        ,CAST(approximate_member_count AS INTEGER)
+        ,CAST(approximate_presence_count AS INTEGER)
+  FROM {{ source('public', 'discord_data') }}
+),
+
+enriched_columns AS (
+  SELECT *
+        ,date_trunc('hour', extracted_at) AS hour_interval
+        ,EXTRACT('ISODOW' FROM date_trunc('hour', extracted_at))::int AS isodow
+  FROM coerced_types
+)
+
+SELECT *
+FROM enriched_columns
